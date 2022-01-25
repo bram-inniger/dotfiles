7ce4437bafe4aa73569457904edacd1b1662eb3e
Bram Inniger <bram@inniger.be>
2022-01-25 18:53:48 +0100

    Fixed order between installed and available

--- configuration/config.fish
+++ configuration/config.fish
@@ -41,8 +41,8 @@ function kernel --description 'Show installed and available kernel versions'
             echo "Installed:" (uname -v | awk -F '[ :]' '{print $4}')
             echo "Available: please run 'softwareupdate -l' for available updates"
         case Linux
-            echo "Installed:" (apt-cache show linux-image-cloud-amd64/unstable | grep Version | awk '{print $2}')
-            echo "Available:" (uname -v | awk '{print $4}')
+            echo "Installed:" (uname -v | awk '{print $4}')
+            echo "Available:" (apt-cache show linux-image-cloud-amd64/unstable | grep Version | awk '{print $2}')
             echo "Last updated:" (apt-get changelog linux-image-cloud-amd64/unstable | grep -e "^ --" | head -n 1 | awk -F '  ' '{print $2}')
         case '*'
             echo "Your OS" (uname) "is not supported by this script"
